a8cd6e420545d151c171b26247f0f68d00d08f08
amitnahtomi <amit.nahtomi@gmail.com>
2022-02-07 22:45:52 +0200

    can get pastes to front

--- pastes-dashboard/src/App.js
+++ pastes-dashboard/src/App.js
@@ -1,9 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+  const [pastes, setPastes] = useState([])
+  
+  useEffect(()=>{
+    const getPastes = async () => {
+      const res = await axios.get('http://localhost:8080/')
+      setPastes(res.data)
+    }
+    getPastes();
+  },[])
+  
+  return <div>
+    <ul>
+      {pastes.map((p)=>{
+        return <li>
+          <span>{p.title}</span>
+          <span>{p.author}</span>
+          <span>{p.date}</span>
+          <div>{p.content}</div>
+        </li>
+      })}
+    </ul>
+  </div>
+  
 }
 
 export default App;
