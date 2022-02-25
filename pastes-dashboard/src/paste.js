61fd7c5b8a8e554c905259a7a5b471bde9ba8f0c
amitnahtomi <amit.nahtomi@gmail.com>
2022-02-25 02:23:28 +0200

    Update paste.js

--- pastes-dashboard/src/paste.js
+++ pastes-dashboard/src/paste.js
@@ -15,39 +15,31 @@ export default function Paste({paste, age, filter, changeFilter}) {
 
     const buttonSym = () => {
         if(visible) {
-            return <ExpandMoreRoundedIcon />
+            return <ExpandMoreRoundedIcon fontSize="large" />
         }
         else {
-            return <ExpandLessRoundedIcon />
+            return <ExpandLessRoundedIcon fontSize="large" />
         }
     }
 
-    if(age >= 18){
-        return <li hidden={!paste.content.toLowerCase().includes(filter) && !paste.title.toLowerCase().includes(filter)}><Card variant="outlined" style={{borderColor: "violet", padding: "8px", backgroundColor: "lightgoldenrodyellow", textAlign: "left"}}>
-            <div style={{textAlign: "right", float: "right", width: "50%"}} hidden={sentiment.analyze(paste.content).score >= 0}><WarningAmberRoundedIcon fontSize="large" style={{height: "13%", width: "13%"}} /></div>
-            <div style={{width: "50%"}}>{paste.title}</div>
-            <div>by {paste.author}</div>
-            <div>at {moment(paste.date).format('LLLL')}</div>
-            <div>sentiment value: {sentiment.analyze(paste.content).score}</div>
-            <Button size="small" variant="outlined" onClick={()=>{setVisible(!visible)}}>{buttonSym()}</Button>
-            <div hidden={visible}>{paste.content}</div>
-            <div style={{textAlign: "right"}}>{paste.entities.map((ent)=>{
-              return <span className="labels" onClick={changeFilter}>{ent}</span>
-            })}</div>
-            </Card></li>
+    const textClean = (text) => {
+        if(age >= 18) {
+            return text
+        }
+        else {
+            return wordFilter.clean(text)
+        }
     }
-    else {
-        return <li hidden={!paste.content.toLowerCase().includes(filter) && !paste.title.toLowerCase().includes(filter)}><Card variant="outlined" style={{borderColor: "violet", padding: "8px", backgroundColor: "lightgoldenrodyellow", textAlign: "left"}}>
+    return <li hidden={!paste.content.toLowerCase().includes(filter) && !paste.title.toLowerCase().includes(filter)}><Card variant="outlined" style={{borderColor: "violet", padding: "8px", backgroundColor: "lightgoldenrodyellow", textAlign: "left"}}>
               <div style={{textAlign: "right", float: "right", width: "50%"}} hidden={sentiment.analyze(paste.content).score >= 0}><WarningAmberRoundedIcon fontSize="large" style={{height: "13%", width: "13%"}} /></div>
-              <div style={{width: "50%"}}>{wordFilter.clean(paste.title)}</div>
+              <div style={{width: "50%"}}>{textClean(paste.title)}</div>
               <div>by {paste.author}</div>
               <div>at {moment(paste.date).format('LLLL')}</div>
               <div>sentiment value: {sentiment.analyze(paste.content).score}</div>
               <Button size="small" variant="outlined" onClick={()=>{setVisible(!visible)}}>{buttonSym()}</Button>
-              <div hidden={visible}>{wordFilter.clean(paste.content)}</div>
+              <div hidden={visible}>{textClean(paste.content)}</div>
               <div style={{textAlign: "right"}}>{paste.entities.map((ent)=>{
-              return <span className="labels" onClick={changeFilter}>{wordFilter.clean(ent)}</span>
+              return <span className="labels" onClick={changeFilter}>{textClean(ent)}</span>
               })}</div>
               </Card></li>
-    }
 }
